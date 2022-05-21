d4e146213524ca3e0c4236657fcbe5ecd9da2b50
zafuso <71095771+zafuso@users.noreply.github.com>
2022-05-21 17:30:52 +0200

    backend commit and remove caching

--- MyCoffeeApp/MyCoffeeApp/Services/InternetCoffeeService.cs
+++ MyCoffeeApp/MyCoffeeApp/Services/InternetCoffeeService.cs
@@ -15,48 +15,28 @@ namespace MyCoffeeApp.Services
 {
     public static class InternetCoffeeService
     {
-        //static string Baseurl = DeviceInfo.Platform == DevicePlatform.Android ?
-        //                                    "http://10.0.2.2:5000" : "http://localhost:5000";
+        static string BaseUrl = DeviceInfo.Platform == DevicePlatform.Android ?
+                                            "http://10.0.2.2:5000" : "http://localhost:5000";
 
-        static string BaseUrl = "YOUR URL";
+        // static string BaseUrl = "YOUR URL";
 
         static HttpClient client;
+        static HttpClientHandler handler;
 
         static InternetCoffeeService()
         {
-            client = new HttpClient
+            HttpClientHandler insecureHandler = GetInsecureHandler();
+            client = new HttpClient(insecureHandler)
             {
                 BaseAddress = new Uri(BaseUrl)
             };
         }
 
-        public static Task<IEnumerable<Coffee>> GetCoffee() =>
-            GetAsync<IEnumerable<Coffee>>("api/Coffee", "getcoffee");
-
-        static async Task<T> GetAsync<T>(string url, string key, int mins = 1, bool forceRefresh = false)
+        public static async Task<IEnumerable<Coffee>> GetCoffee()
         {
-            var json = string.Empty;
-
-            if (Connectivity.NetworkAccess != NetworkAccess.Internet)
-                json = Barrel.Current.Get<string>(key);
-            else if (!forceRefresh && !Barrel.Current.IsExpired(key))
-                json = Barrel.Current.Get<string>(key);
-
-            try
-            {
-                if (string.IsNullOrWhiteSpace(json))
-                {
-                    json = await client.GetStringAsync(url);
-
-                    Barrel.Current.Add(key, json, TimeSpan.FromMinutes(mins));
-                }
-                return JsonConvert.DeserializeObject<T>(json);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Unable to get information from server {ex}");
-                throw ex;
-            }
+            var json = await client.GetStringAsync("api/Coffee");
+            var coffees = JsonConvert.DeserializeObject<IEnumerable<Coffee>>(json);
+            return coffees;
         }
 
         static Random random = new Random();
@@ -79,7 +59,7 @@ namespace MyCoffeeApp.Services
 
             if(!response.IsSuccessStatusCode)
             {
-
+                _ = Xamarin.Forms.Application.Current.MainPage.DisplayAlert("Failed", "Failed to post coffee", "cancel");
             }
         }
 
@@ -92,6 +72,16 @@ namespace MyCoffeeApp.Services
             }
         }
 
-        
+        public static HttpClientHandler GetInsecureHandler()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+            {
+                if (cert.Issuer.Equals("CN=localhost"))
+                    return true;
+                return errors == System.Net.Security.SslPolicyErrors.None;
+            };
+            return handler;
+        }
     }
 }
